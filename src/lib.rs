a68ca5cdf4aa88a6033d368a88c28a186b339b44
Michael Yuan <michael@michaelyuan.com>
2020-03-20 02:02:34 -0500

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -5,14 +5,11 @@ pub fn say(s: String) -> String {
 
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-    
-    #[test]
-    fn say_hello() {
-        let result = say("ssvm");
-        assert!(result.contains("hello ssvm"));
-    }
+  use super::*;
+  
+  #[test]
+  fn say_hello() {
+    let result = say("ssvm");
+    assert!(result.contains("hello ssvm"));
+  }
 }
