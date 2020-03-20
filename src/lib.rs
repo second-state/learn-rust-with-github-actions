01ad0ba3b439d9264c6f8a098420b30b1b8c6409
Michael Yuan <michael@michaelyuan.com>
2020-03-19 22:17:22 -0500

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,11 @@ pub fn say(s: String) -> String {
   let r = String::from("hello ");
   return r + &s;
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
