115909fe6b566a6cb08a7e89dc61048bed35ad4d
Michael Yuan <michael@secondstate.io>
2020-05-11 01:59:14 +0000

    Add next steps

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
     println!("Hello, world!");
 }
+
+// Next, try a more interesting example from the interactive Terminal: https://github.com/glitteringkatie/spongemock
+
+// Learn Rust from examples: https://rust-by-example-ext.com/
